a6909d7deb468f6dddfadcaab98ef4b8affaf018
emericgautier <emericgtr@gmail.com>
2022-10-08 16:51:43 +0200

    display data on screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Card from "./components/Card";
 
 const App = () => {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    // console.log("test");
+    axios
+      .get("https://randomuser.me/api/?results=24")
+      .then((res) => setUserData(res.data.results));
+  }, []);
+
   return (
     <div>
       <h1>Users</h1>
-      <Card />
+      <div className="cards">
+        {userData &&
+          userData.map((user, index) => <Card key={index} user={user} />)}
+      </div>
     </div>
   );
 };
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,16 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ user }) => {
   return (
-    <div className="cards">
-      <div className="card">
-        <img src="https://randomuser.me/api/portraits/women/33.jpg" alt="" />
-        <h3>Marie-Luise Kamp</h3>
-        <p>Pocking, 13 avril 1977</p>
-        <em>Membre depuis : 497 jours</em>
-      </div>
-      <div className="card">
-        <img src="https://randomuser.me/api/portraits/women/33.jpg" alt="" />
-        <h3>Marie-Luise Kamp</h3>
-        <p>Pocking, 13 avril 1977</p>
-        <em>Membre depuis : 497 jours</em>
-      </div>
-      <div className="card">
-        <img src="https://randomuser.me/api/portraits/women/33.jpg" alt="" />
-        <h3>Marie-Luise Kamp</h3>
-        <p>Pocking, 13 avril 1977</p>
-        <em>Membre depuis : 497 jours</em>
-      </div>
+    <div className="card">
+      <img src={user.picture.large} alt={user.name.first} />
+      <h3>
+        {user.name.first} {user.name.last}
+      </h3>
+      <p>
+        {user.location.country}, {user.dob.date}
+      </p>
+      <em>Membre depuis : {user.registered.date}</em>
     </div>
   );
 };
