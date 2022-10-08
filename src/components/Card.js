8dd7f06b26daa5e5e2a42919140f7a94d36c6826
emericgautier <emericgtr@gmail.com>
2022-10-08 15:39:34 +0200

    Create Card.js and _card.scss

---
+++ src/components/Card.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="cards">
+      <div className="card">
+        <img src="https://randomuser.me/api/portraits/women/33.jpg" alt="" />
+        <h3>Marie-Luise Kamp</h3>
+        <p>Pocking, 13 avril 1977</p>
+        <em>Membre depuis : 497 jours</em>
+      </div>
+      <div className="card">
+        <img src="https://randomuser.me/api/portraits/women/33.jpg" alt="" />
+        <h3>Marie-Luise Kamp</h3>
+        <p>Pocking, 13 avril 1977</p>
+        <em>Membre depuis : 497 jours</em>
+      </div>
+      <div className="card">
+        <img src="https://randomuser.me/api/portraits/women/33.jpg" alt="" />
+        <h3>Marie-Luise Kamp</h3>
+        <p>Pocking, 13 avril 1977</p>
+        <em>Membre depuis : 497 jours</em>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
