ee1a91cab7594980e6703ee0bb94cc8f4446f7e8
emericgautier <emericgtr@gmail.com>
2022-10-08 15:39:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Card from "./components/Card";
 
 const App = () => {
   return (
     <div>
-      <h1>REACT</h1>
+      <h1>Users</h1>
+      <Card />
     </div>
   );
 };
