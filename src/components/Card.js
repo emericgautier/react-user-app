3b2cd0296362d823003d2a6367b7dfb9f8a2b634
emericgautier <emericgtr@gmail.com>
2022-10-08 18:15:35 +0200

    convert date to iso

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 const Card = ({ user }) => {
+  // traiter les dates en ISO
+  const dateParser = (date) => {
+    let newDate = new Date(date).toLocaleDateString("fr-FR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    return newDate;
+  };
+
   return (
     <div className="card">
       <img src={user.picture.large} alt={user.name.first} />
@@ -8,7 +18,7 @@ const Card = ({ user }) => {
         {user.name.first} {user.name.last}
       </h3>
       <p>
-        {user.location.country}, {user.dob.date}
+        {user.location.country}, {dateParser(user.dob.date)}
       </p>
       <em>Membre depuis : {user.registered.date}</em>
     </div>
